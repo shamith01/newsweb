37f091c860d488d664335536928d4d1eec4f6906
shamith01 <128206892+shamith01@users.noreply.github.com>
2023-04-02 17:00:33 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.9)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      },
+      height:{
+        "98":'500px',
+      },
+      width:{
+        "08":'263px',
+        "21":"5.4rem",
+        "98":'500px',
+      },
+    },
+  },
+  plugins: [],
+}
+
